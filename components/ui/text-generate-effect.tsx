0305bd8344ff77102f57c71436b2a8c13e849d1b
RexO77 <nischal.skanda07@gmail.com>
2024-05-29 22:20:47 +0530

    Day-6

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -57,3 +57,8 @@ export const TextGenerateEffect = ({
 // Compare this snippet from components/Hero.tsx:
 // import React from 'react'
 // import { Spotlight } from './ui/Spotlight'
+//    
+// const Hero = () => {
+//   return (
+//     <div className='pb-20 pt-36'>
+//         <div>
