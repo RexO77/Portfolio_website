585c39b70c7d6ab48c0b5d2b11928415242875dd
RexO77 <nischal.skanda07@gmail.com>
2024-05-31 23:02:17 +0530

    Day-8

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,6 +17,7 @@ const Hero = () => {
             <div className='max-w-[89vw]'>
               <h2>Mastering the bridge between Design and Devolopement</h2>
               <p>Design and Devolopment are two sides of the same coin. We help you master both.</p>
+              <button>Learn More</button>
             </div>
 
           </div>
