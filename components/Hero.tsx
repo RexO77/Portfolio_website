831c395469445f89be432fae43b4a502264bef2d
RexO77 <nischal.skanda07@gmail.com>
2024-06-14 19:55:48 +0530

    Day-19

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,9 +13,9 @@ const Hero = () => {
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
           </div>
 
-          <div className='flex justify-center'>
-            <div className='max-w-[89vw]'>
-              <h2>Mastering the bridge between Design and Devolopement</h2>
+          <div className='flex justify-center relative my-20 z-10'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+              <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Mastering the bridge between Design and Devolopement</h2>
               <p className='text-lg'>We are a team of talented designers making websites with Bootstrap</p>
               <p>Just a routine commit feel free to remove this up </p>
             </div>
