022a65e87a0dcd619edba6056af81559a3f2200a
RexO77 <nischal.skanda07@gmail.com>
2024-06-14 19:57:41 +0530

    Day-19

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <Spotlight className='-top-10 -left-full md:-left-32 h-[80vh] w-[50vw]' fill='purple' />
             <Spotlight className='-top-28 -left-80 h-[80vh] w-[50vw]' fill='blue' />
         </div>
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center absolute top-0 left-0">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]">
           </div>
 
@@ -17,7 +17,6 @@ const Hero = () => {
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
               <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Mastering the bridge between Design and Devolopement</h2>
               <p className='text-lg'>We are a team of talented designers making websites with Bootstrap</p>
-              <p>Just a routine commit feel free to remove this up </p>
             </div>
 
           </div>
