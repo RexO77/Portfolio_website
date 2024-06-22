043381ccc8baf5bab99a1e1f5a325c951fab50ca
RexO77 <nischal.skanda07@gmail.com>
2024-06-22 22:45:16 +0530

    Day-27

--- components/ui/Spotlight.tsx
+++ components/ui/Spotlight.tsx
@@ -54,4 +54,4 @@ export const Spotlight = ({ className, fill }: SpotlightProps) => {
     </svg>
   );
 };
-//daily push onto github Now another one last push
+//daily push onto github Now another one last push for today
